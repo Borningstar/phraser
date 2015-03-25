1868932eecf44aa063554881cd7cb9f8292be9ce
Ben Morningstar <ben@morningstar.id.au>
2015-03-25 17:07:39 +1000

    Changes to various files

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 var db = require('./database');
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var agents = require('./routes/agents');
+var phrases = require('./routes/phrases');
 
 var app = express();
 
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/agents', agents);
+app.use('/phrases', phrases);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- models/phrase.js
+++ models/phrase.js
@@ -1,16 +1,17 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var sentenceSchema = new Schema({
-  words: [{	id: { type: String, index: true },
+var phraseSchema = new Schema({
+	name: String,
+  	words: [{	id: { type: String, index: true },
   		list: [String],
   		enabled: { type: bool, default: true },
   		next: [String] }],
-  lists: [{	id:  type: String, index: true },
+	lists: [{	id:  type: String, index: true },
 		list: [String] }]
 });
 
-sentenceSchema.methods.getWords = function (id) {
+phraseSchema.methods.getWords = function (id) {
 	var word = null;
 	var words = this.words[id].list;
 
@@ -21,7 +22,7 @@ sentenceSchema.methods.getWords = function (id) {
 	return word;
 };
 
-sentenceSchema.methods.getNext = function (id) {
+phraseSchema.methods.getNext = function (id) {
 	var next = null;
 	var nextList = this.words.next;
 	var words = this.words.words;	
@@ -38,6 +39,6 @@ sentenceSchema.methods.getNext = function (id) {
 	return next;
 };
 
-var Sentence = mongoose.model('Sentence', sentenceSchema);
+var Phrase = mongoose.model('Phrase', phraseSchema);
 
-module.exports = Sentence;
+module.exports = Phrase;
